2491812589cb8633284354b436b9b384e419df53
hrd2910 <35139001+hrd2910@users.noreply.github.com>
2018-02-14 23:25:21 +0530

    Add files via upload

---
+++ longestWord.py
@@ -0,0 +1,10 @@
+#Implement a function longestWord() that takes a list of words and returns the longest one.
+
+def find_longest_word(words_list):
+    word_len = []
+    for n in words_list:
+        word_len.append((len(n), n))
+    word_len.sort()
+    return word_len[-1][1]
+
+print(find_longest_word(["PHP", "Python", "Java"]))
